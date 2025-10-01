16406f1156d7eb72b6bd2ef31778bc751277155e
junxxx <hprjunxxx@gmail.com>
2025-10-01 20:24:16 +0800

    import element-ui css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui';
 
 import './assets/main.css'
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(PiniaVuePlugin)
 Vue.use(ElementUI)
