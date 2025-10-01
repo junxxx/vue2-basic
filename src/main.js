087974eccf80f4b96b9cde6031c75b8f5d133de9
junxxx <hprjunxxx@gmail.com>
2025-10-01 14:32:29 +0800

    add a dashboard component with Element UI

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createPinia, PiniaVuePlugin } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
 
 import './assets/main.css'
 
 Vue.use(PiniaVuePlugin)
+Vue.use(ElementUI)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 Vue.use(VueRouter)
 
@@ -12,6 +13,11 @@ const router = new VueRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
     },
     {
       path: '/about',
